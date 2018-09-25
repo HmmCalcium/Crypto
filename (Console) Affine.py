3977834bb5d3d0883ec9ae59a9c5099e0998388f
Alex <16scorzaa@cedarsupper.co.uk>
2018-09-25 16:52:21 +0100

    Now allows trig

--- (Console) Affine.py
+++ (Console) Affine.py
@@ -1,10 +1,11 @@
-#Alex Scorza
+#Alex Scorza September 2018
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+from math import *
 
 while 1:
     text = input("Enter some text to encode\n> ")
-    func_text = input("(With x as letter number, auto does %26) enter and expression\n> ")
-    #i.e (5*x)+8
+    func_text = input("(With x as letter number, auto does %26, done 'from math import *', trig expects answers in radians) enter and expression\n> ")
+    #i.e sin(deg(5*x)+8
     func = lambda x: eval(func_text)
     answer = ""
     for x in text:
