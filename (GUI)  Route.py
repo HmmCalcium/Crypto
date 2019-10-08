c62762b83394914f1fed250537bf5ac54d974dd0
MadLad <41120880+MegaDoot@users.noreply.github.com>
2019-10-08 15:13:32 +0100

    Fixed mistake with dimensions, more buttons

--- (GUI)  Route.py
+++ (GUI)  Route.py
@@ -1,3 +1,5 @@
+# https://stackoverflow.com/questions/3085696/adding-a-scrollbar-to-a-group-of-widgets-in-tkinter
+
 import tkinter as tk
 import tkinter.scrolledtext as tkst
 
@@ -18,7 +20,6 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-
         # Widgets
         self.enter = tkst.ScrolledText(
             self, width=100, height=10, spacing2=10, **style())
@@ -29,25 +30,36 @@ class App(tk.Tk):
             self.under_enter_frame, variable=self.skip_length, from_=1, to=10,
             orient="horizontal", relief="flat", sliderrelief="flat",
             tickinterval=9, command=self.update_grid, **style(13))
+        self.key_entry = tk.Entry(self.under_enter_frame, width=10, **style())
 
         self.scroll_canvas = tk.Canvas(height=300, bg="red")
         self.grid_frame = tk.Frame(self)
+        self.scrollbar = tk.Scrollbar(self, orient="horizontal", command=self.scroll_canvas.xview())
+        self.scroll_canvas.configure(xscrollcommand=self.scrollbar.set)
 
         # Grid
         self.enter.grid(row=0, column=0)
 
         self.under_enter_frame.grid(row=1, column=0)
-        self.skip_slider.grid(row=0, column=1)
+        self.skip_slider.grid(row=0, column=0)
+        self.key_entry.grid(row=1, column=0)
 
-        #self.scroll_canvas.grid(row=2, column=0, sticky="EW")
-        self.grid_frame.grid(row=2, column=0, sticky="NESW")
+        self.scroll_canvas.grid(row=0, column=1, sticky="NESW")
+        
         self.update_idletasks()
         width = self.scroll_canvas.winfo_width()
         height = self.scroll_canvas.winfo_height()
-        #self.scroll_canvas.create_window(
-            #(width / 2, height / 2), window=self.grid_frame, anchor="center")
+        self.scroll_canvas.create_window(
+            (width / 2, height / 2), window=self.grid_frame,
+            height=height, width=width)
+
+        # Binding
+        self.grid_frame.bind("<Configure>", self.on_frame_config)
 
         self.mainloop()
+
+    def on_frame_config(self, event):
+        self.scroll_canvas.configure(scrollregion=self.scroll_canvas.bbox("all"))
 
     def clear_weight(self, frame):
         columns, rows = frame.grid_size()
@@ -57,23 +69,19 @@ class App(tk.Tk):
             frame.grid_columnconfigure(r, weight=0)
 
     def update_grid(self, event=None):
-        rows = self.skip_length.get()
+        columns = self.skip_length.get()
         text = self.enter.get("1.0", "end").replace("\n", "").replace(" ", "")
-        fill_len = (rows - (len(text) % rows)) % rows
+        fill_len = (columns - (len(text) % columns)) % columns
         text += "X" * fill_len
 
         # Delete current
         for widget in self.grid_frame.winfo_children():
             widget.destroy()
 
-        letters = [[] for _ in range(len(text) // rows)]
-        for r in range(rows):
-            temp_list = []
-            for c in range(r, len(text), rows):
-                temp_list.append(text[c])
-            for i in range(len(temp_list)):
-                # This makes each list into a column not row
-                letters[i].append(temp_list[i])
+        letters = [[] for _ in range(columns)]
+        for c in range(columns):
+            for r in range(c, len(text), columns):
+                letters[c].append(text[r])
         for column in range(len(letters)):
             for row in range(len(letters[column])):
                 tk.Label(
